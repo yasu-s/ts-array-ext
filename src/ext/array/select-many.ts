e2ac008300a02dfc85b90c8cbb8107ef139015aa
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2018-08-04 16:15:02 +0900

    selectMany追加

---
+++ src/ext/array/select-many.ts
@@ -0,0 +1,20 @@
+export {};
+
+declare global {
+  interface Array<T> {
+    /**
+     * [拡張メソッド]
+     * callbackfnで戻した配列を結合します
+     * @param callbackfn 条件
+     * @return 結合後の配列
+     */
+    selectMany<U>(callbackfn: (value: T, index: number, array: T[]) => U[]): U[];
+  }
+}
+
+Array.prototype.selectMany = function<T, U>(callbackfn: (value: T, index: number, array: T[]) => U[]): U[] {
+  const items = this as T[];
+  if (!Array.isArray(items) || items.length === 0) return [];
+
+  return items.map(callbackfn).reduce((prev, current) => prev.concat(current));
+};
---
+++ test/select-many-test.ts
@@ -0,0 +1,26 @@
+import '../src/ext/array/select-many';
+
+describe('Array.selectMany', () => {
+  it('Array.length > 0', () => {
+    // setup
+    const items = [ [1, 2, 3], [4, 5] ];
+
+    // execrise
+    const actual = items.selectMany((value) => value);
+
+    // verify
+    expect(actual.length).toBe(5);
+    expect(actual).toEqual([ 1, 2, 3, 4, 5]);
+  });
+
+  it('Array.length = 0', () => {
+    // setup
+    const items = [];
+
+    // execrise
+    const actual = items.selectMany((value) => value);
+
+    // verify
+    expect(actual.length).toBe(0);
+  });
+});

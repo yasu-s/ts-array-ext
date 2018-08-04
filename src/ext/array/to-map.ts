57d7b4d725f9007482f9212c99d20c14ad39de93
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2018-08-04 14:50:00 +0900

    toMap追加

---
+++ src/ext/array/to-map.ts
@@ -0,0 +1,28 @@
+export {};
+
+declare global {
+  interface Array<T> {
+    /**
+     * 配列をMapに変換します
+     * @param keyFn
+     * @param valueFn
+     * @return Map
+     */
+    toMap<K, V>(keyFn: (value: T) => K, valueFn: (value: T) => V): Map<K, V>;
+  }
+}
+
+Array.prototype.toMap = function<T, K, V>(keyFn: (value: T) => K, valueFn: (value: T) => V): Map<K, V> {
+  const items = this as T[];
+  if (!Array.isArray(items)) return null;
+
+  const convItems = items.map(item => { return { key: keyFn(item), value: valueFn(item) }; });
+
+  const map = new Map<K, V>();
+  for (const item of convItems.filter((item, index, array) => array.indexOf(item) === index)) {
+    if (!map.has(item.key))
+      map.set(item.key, item.value);
+  }
+
+  return map;
+};
--- src/ext/index.ts
+++ src/ext/index.ts
@@ -1,6 +1,7 @@
 /* tslint:disable */
 import './array/any';
+import './array/distinct';
 import './array/first';
 import './array/last';
 import './array/orderby';
-
+import './array/to-map';

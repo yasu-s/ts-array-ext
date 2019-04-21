f54a4c00330d1a05c7c6f099c2561154c992d0b7
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2019-04-21 21:21:42 +0900

    test実装

---
+++ test/trim-test.ts
@@ -0,0 +1,47 @@
+import '../src/ext/array/trim';
+
+describe('Array.trim', () => {
+  it('null', () => {
+    // setup
+    const items = [1, null, 3];
+
+    // exercise
+    const actual = items.trim();
+
+    // verify
+    expect(actual).toEqual([1, 3]);
+  });
+
+  it('undefined', () => {
+    // setup
+    const items = [1, undefined, 3];
+
+    // exercise
+    const actual = items.trim();
+
+    // verify
+    expect(actual).toEqual([1, 3]);
+  });
+
+  it('null or undefined nothing', () => {
+    // setup
+    const items = [1, 2, 3];
+
+    // exercise
+    const actual = items.trim();
+
+    // verify
+    expect(actual).toEqual([1, 2, 3]);
+  });
+
+  it('Array.length = 0', () => {
+    // setup
+    const items = [];
+
+    // exercise
+    const actual = items.trim();
+
+    // verify
+    expect(actual).toEqual([]);
+  });
+});

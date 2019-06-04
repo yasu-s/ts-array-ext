393751aa0f56d63ae4fa09c70738c66db9292290
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2019-06-04 21:17:14 +0900

    test実装

---
+++ test/to-set-test.ts
@@ -0,0 +1,55 @@
+import '../src/ext/array/to-set';
+
+class TestData {
+  id: number;
+  name: string;
+  memo: string;
+}
+
+describe('Array.toSet', () => {
+  let testData: TestData[];
+
+  beforeEach(() => {
+    testData = new Array<TestData>();
+
+    let d = new TestData();
+    d.id = 3;
+    d.name = 'Bob';
+    testData.push(d);
+
+    d = new TestData();
+    d.id = 2;
+    d.name = 'Char';
+    testData.push(d);
+
+    d = new TestData();
+    d.id = 4;
+    d.name = 'Alex';
+    testData.push(d);
+
+    d = new TestData();
+    d.id = 1;
+    d.name = 'Bob';
+    testData.push(d);
+  });
+
+  it('key = id', () => {
+    // exercise
+    const actual = testData.toSet((item) => item.id);
+
+    // verify
+    expect(actual.size).toBe(4);
+    expect(actual.has(1)).toBeTruthy();
+    expect(actual.has(100)).toBeFalsy();
+  });
+
+  it('key = name', () => {
+    // exercise
+    const actual = testData.toSet((item) => item.name);
+
+    // verify
+    expect(actual.size).toBe(3);
+    expect(actual.has('Bob')).toBeTruthy();
+    expect(actual.has('Bub')).toBeFalsy();
+  });
+});

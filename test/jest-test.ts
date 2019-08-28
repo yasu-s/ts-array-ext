2505ed4b24ef169c08735b169b7eb0f6f4ae40b2
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2019-08-28 21:17:29 +0900

    jest mock test

---
+++ test/jest-test.ts
@@ -0,0 +1,23 @@
+describe('jest', () => {
+  it('Date.now', () => {
+    // setup
+    Date.now = jest.fn().mockReturnValue(new Date(2019, 1, 1).valueOf());
+
+    // exercise
+    const actual = Date.now();
+
+    // verify
+    expect(actual).toBe(new Date(2019, 1, 1).valueOf());
+  });
+
+  it('Date.now not mock', () => {
+    // setup
+    jest.resetAllMocks();
+
+    // exercise
+    const actual = Date.now();
+
+    // verify
+    expect(actual).not.toBe(new Date(2019, 1, 1).valueOf());
+  });
+});

52cd4c3c410b577c2542b71e7e67f6b5d9b5d13b
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2019-08-28 21:26:26 +0900

    mockDate override

--- test/jasmine-test.ts
+++ test/jasmine-test.ts
@@ -9,4 +9,20 @@ describe('Jasmine', () => {
     // verify
     expect(hoge).toHaveBeenCalled();
   });
+
+  it('clock', () => {
+    // setup
+    jasmine.clock = jest.fn().mockReturnValue({
+      mockDate: (date?: Date) => {
+        Date.now = jest.fn().mockReturnValue(date.valueOf());
+      }
+    });
+    jasmine.clock().mockDate(new Date(2019, 1, 1));
+
+    // exercise
+    const actual = Date.now();
+
+    // verify
+    expect(actual).toBe(new Date(2019, 1, 1).valueOf());
+  });
 });

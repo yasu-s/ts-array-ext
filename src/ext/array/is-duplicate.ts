3bf2a98bbb9ba122a1304929f5a40ad8711d9f3e
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2020-02-08 10:33:35 +0900

    重複判定

---
+++ src/ext/array/is-duplicate.ts
@@ -0,0 +1,19 @@
+export {};
+
+declare global {
+  interface Array<T> {
+    /**
+     * [拡張メソッド]
+     * 対象のインデックスの値が重複しているか判定します。
+     * @param index 対象のインデックス
+     * @return 重複していた場合、true
+     */
+    isDuplicate(index: number): boolean;
+  }
+}
+
+Array.prototype.isDuplicate  = function<T>(index: number): boolean {
+  const items = this as T[];
+  if (!Array.isArray(items) || items.length === 0 || index < 0 || items.length <= index) return false;
+  return items.some((item, i) => i !== index && items[index] === item);
+};
--- src/ext/index.ts
+++ src/ext/index.ts
@@ -6,6 +6,7 @@ import './array/deep-copy';
 import './array/distinct';
 import './array/equal-to';
 import './array/first';
+import './array/is-duplicate';
 import './array/last';
 import './array/orderby';
 import './array/remove';

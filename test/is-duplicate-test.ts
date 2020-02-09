228a10e5f9afae3f165eb90dfb7557d6c491c573
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2020-02-09 10:35:49 +0900

    UT

---
+++ test/is-duplicate-test.ts
@@ -0,0 +1,63 @@
+import '../src/ext/array/is-duplicate';
+
+describe('Array.distinct', () => {
+  it('duplicate', () => {
+    // setup
+    const index = 0;
+    const items = [1, 2, '1', 1, '2', '1'];
+
+    // exercise
+    const actual = items.isDuplicate(index);
+
+    // verify
+    expect(actual).toBeTruthy();
+  });
+
+  it('no duplicate', () => {
+    // setup
+    const index = 1;
+    const items = [1, 2, '1', 1, '2', '1'];
+
+    // exercise
+    const actual = items.isDuplicate(index);
+
+    // verify
+    expect(actual).toBeFalsy();
+  });
+
+  it('items.length = 0', () => {
+    // setup
+    const index = 0;
+    const items = [] as any[];
+
+    // exercise
+    const actual = items.isDuplicate(index);
+
+    // verify
+    expect(actual).toBeFalsy();
+  });
+
+  it('items.length < 0', () => {
+    // setup
+    const index = -1;
+    const items = [1, 2, 1];
+
+    // exercise
+    const actual = items.isDuplicate(index);
+
+    // verify
+    expect(actual).toBeFalsy();
+  });
+
+  it('items.length <= index', () => {
+    // setup
+    const index = 3;
+    const items = [1, 2, 1];
+
+    // exercise
+    const actual = items.isDuplicate(index);
+
+    // verify
+    expect(actual).toBeFalsy();
+  });
+});

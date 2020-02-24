95b6582741fefa2511e24e64111b29823e0ad78a
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2020-02-24 21:14:40 +0900

    UT消化

--- test/zip-test.ts
+++ test/zip-test.ts
@@ -12,4 +12,40 @@ describe('Array.zip', () => {
     // verify
     expect(actual).toEqual(['1a', '2b', '3c']);
   });
+
+  it('second = null', () => {
+    // setup
+    const items = [1, 2, 3];
+    const second = null as any;
+
+    // exercise
+    const actual = items.zip(second, (f, s) => `${f}${s}`);
+
+    // verify
+    expect(actual).toEqual([]);
+  });
+
+  it('fn = null', () => {
+    // setup
+    const items = [1, 2, 3];
+    const second = ['a', 'b', 'c'];
+
+    // exercise
+    const actual = items.zip(second, null as any);
+
+    // verify
+    expect(actual).toEqual([]);
+  });
+
+  it('items.length < second.length', () => {
+    // setup
+    const items = [1, 2];
+    const second = ['a', 'b', 'c'];
+
+    // exercise
+    const actual = items.zip(second, (f, s) => `${f}${s}`);
+
+    // verify
+    expect(actual).toEqual(['1a', '2b']);
+  });
 });

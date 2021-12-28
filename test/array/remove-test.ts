0cafcb492d642c63e840ff7aef9e8cd32fb4d86e
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2021-12-28 10:10:15 +0900

    リファクタ

--- test/array/remove-test.ts
+++ test/array/remove-test.ts
@@ -1,49 +1,27 @@
 import '../../src/ext/array/remove';
 
 describe('Array.remove', () => {
-  it('ok', () => {
-    // setup
-    const items = [1, 2, 3];
-
+  it.each([
+    { case: 'ok', items: [1, 2, 3], target: 2, expected: [1, 3] },
+    { case: 'not found', items: [1, 2, 3], target: 4, expected: [1, 2, 3] },
+  ])('$case', ({ items, target, expected }) => {
     // exercise
-    items.remove(2);
+    items.remove(target);
 
     // verify
-    expect(items).toEqual([1, 3]);
-  });
-
-  it('not found', () => {
-    // setup
-    const items = [1, 2, 3];
-
-    // exercise
-    items.remove(4);
-
-    // verify
-    expect(items).toEqual([1, 2, 3]);
+    expect(items).toEqual(expected);
   });
 });
 
 describe('Array.removeAt', () => {
-  it('ok', () => {
-    // setup
-    const items = [1, 2, 3];
-
+  it.each([
+    { case: 'ok', items: [1, 2, 3], index: 1, expected: [1, 3] },
+    { case: 'not found', items: [1, 2, 3], index: 4, expected: [1, 2, 3] },
+  ])('$case', ({ items, index, expected }) => {
     // exercise
-    items.removeAt(1);
+    items.removeAt(index);
 
     // verify
-    expect(items).toEqual([1, 3]);
-  });
-
-  it('ok', () => {
-    // setup
-    const items = [1, 2, 3];
-
-    // exercise
-    items.removeAt(4);
-
-    // verify
-    expect(items).toEqual([1, 2, 3]);
+    expect(items).toEqual(expected);
   });
 });

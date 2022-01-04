628b31698c11ab6694598c570dc74c924680e8cc
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2022-01-04 11:06:53 +0900

    リファクタ

--- test/object/equal-to-test.ts
+++ test/object/equal-to-test.ts
@@ -2,54 +2,34 @@ import '../../src/ext/object/equal-to';
 
 describe('Object.equalTo', () => {
   describe('object', () => {
-    it('ok', () => {
+    it.each([
+      { case: 'ok', target: { id: 1, name: 'hoge' }, expected: true },
+      { case: 'ng', target: { id: 2, name: 'hoge' }, expected: false },
+    ])('$case', ({ target, expected }) => {
       // setup
       const obj = { id: 1, name: 'hoge' };
-      const target = { id: 1, name: 'hoge' };
 
       // exercise
       const actual = obj.equalTo(target);
 
       // verify
-      expect(actual).toBeTruthy();
-    });
-
-    it('ng', () => {
-      // setup
-      const obj = { id: 1, name: 'hoge' };
-      const target = { id: 2, name: 'hoge' };
-
-      // exercise
-      const actual = obj.equalTo(target);
-
-      // verify
-      expect(actual).toBeFalsy();
+      expect(actual).toBe(expected);
     });
   });
 
   describe('nested object', () => {
-    it('ok', () => {
+    it.each([
+      { case: 'ok', target: { data: [{ id: 1, name: 'hoge' }] }, expected: true },
+      { case: 'ng', target: { data: [{ id: 2, name: 'hoge' }] }, expected: false },
+    ])('$case', ({ target, expected }) => {
       // setup
       const obj = { data: [{ id: 1, name: 'hoge' }] };
-      const target = { data: [{ id: 1, name: 'hoge' }] };
 
       // exercise
       const actual = obj.equalTo(target);
 
       // verify
-      expect(actual).toBeTruthy();
-    });
-
-    it('ng', () => {
-      // setup
-      const obj = { data: [{ id: 1, name: 'hoge' }] };
-      const target = { data: [{ id: 2, name: 'hoge' }] };
-
-      // exercise
-      const actual = obj.equalTo(target);
-
-      // verify
-      expect(actual).toBeFalsy();
+      expect(actual).toBe(expected);
     });
   });
 });
